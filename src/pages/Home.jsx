30f9d406eea53ccc603fb430d8207b622b6d9518
Muhammad Usman <musman0741@gmail.com>
2023-10-21 08:22:44 +0500

    [master] Home component, code indentation fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-// Internal Components 
+// Internal Components
 import Navigation from "../components/Navigation";
 import HeroHeader from "../components/HeroHeader";
 import Work from "../components/Work";
@@ -6,15 +6,15 @@ import GetInTouch from "../components/GetInTouch";
 import Footer from "../components/Footer";
 
 const Home = () => {
-    return (
-        <div>
-            <Navigation textColor='white' />
-            <HeroHeader />
-            <Work />
-            <GetInTouch />
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navigation textColor="white" />
+      <HeroHeader />
+      <Work />
+      <GetInTouch />
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
