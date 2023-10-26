b52ed7dc19949d43b5452f0a7f82ff48eb9129cf
Muhammad Usman <musman0741@gmail.com>
2023-10-26 16:35:02 +0500

    [master] new project "pokemon-app" added

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -23,30 +23,40 @@ export const formatAMPM = (date) => {
 export const Sleep = (ms = 100) =>
   new Promise((resolve) => setTimeout(resolve, ms));
 
-
 // Work projects
 export const projects = [
+  {
+    id: 0,
+    image: "pokemon",
+    title: "Pokémon-App",
+    alt: "pokemon",
+    description:
+      "Pokémon-App: Unleash the Pokémon enthusiast in you! Create captivating Pokémon cards instantly by simply entering a Pokémon's name. Experience the magic of personalized card designs at your fingertips.",
+    url: "https://pokemon-app-one-pi.vercel.app/",
+  },
   {
     id: 1,
     image: "monstyr-website",
     title: "Monstyr Website",
     alt: "monstyr-website",
-    description: 'Your ultimate lifestyle-enhancing app for the freshest, hottest deals and experiences. Discover online promos, in-store bargains, dining deals, and more. Customizable and location-based for your convenience!',
+    description:
+      "Your ultimate lifestyle-enhancing app for the freshest, hottest deals and experiences. Discover online promos, in-store bargains, dining deals, and more. Customizable and location-based for your convenience!",
     url: "https://www.monstyr.com/",
   },
   {
     id: 2,
     image: "monstyr-mobile-app",
-    title: 'Monstyr Mobile-App',
+    title: "Monstyr Mobile-App",
     alt: "monstyr-mobile",
-    description: "Discover Monstyr's mobile app for Android & iOS! Unleash the power of convenience in your pocket. Explore diverse deals, stay updated with local promotions, and customize your experience effortlessly.",
+    description:
+      "Discover Monstyr's mobile app for Android & iOS! Unleash the power of convenience in your pocket. Explore diverse deals, stay updated with local promotions, and customize your experience effortlessly.",
     url: "https://play.google.com/store/apps/details?id=com.monstyr",
   },
   {
     id: 3,
     image: "motive",
     title: "Motive formerly KeepTruckin",
-    alt: 'gomotive',
+    alt: "gomotive",
     description:
       "Your fleet's game-changer. Trusted by 120,000+ businesses for safety, productivity, and profitability. Experience AI-powered dashcams, ELD compliance, real-time tracking, and smart spend management. A brand that delivers excellence.",
     url: "https://gomotive.com/",
@@ -55,23 +65,15 @@ export const projects = [
     id: 4,
     image: "artist-events",
     title: "Artist Events",
-    alt: 'artist-events',
+    alt: "artist-events",
     description:
       "Discover global artist events at Artist Events. Unite through music, as it connects people worldwide. Join top events like Taylor Swift, Maroon 5 and more!",
     url: "https://artistevents.netlify.app/",
   },
-  // {
-  //   id: 5,
-  //   image: "pokemon",
-  //   title: "Pokémon-App",
-  //   alt: "pokemon",
-  //   description: 'Make 3D Pokemon Card',
-  //   url: "https://www.google.com/",
-  // },
   {
     id: 6,
     image: "investment",
-    title: 'Saving & Investments',
+    title: "Saving & Investments",
     alt: "investment",
     description:
       "A Finncial Tool that helps you save your money all in one place, vester helps you achieve your financial dreams",
